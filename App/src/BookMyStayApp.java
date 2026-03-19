947e6d79feac89fc4b2de74a4fcd2c10834b361f
Shiv-angiDubey <hellokittyyyy098@gmail.com>
2026-03-19 23:06:34 +0530

    Shivangi added UC2

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,6 +1,70 @@
+abstract class Room {
+
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    // Constructor
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    // Method to display details
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+// Single Room
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+// Double Room
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+// Suite Room
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
 public class BookMyStayApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
-    }
+        Room single = new SingleRoom();
+        Room doubleroom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+    int singleAvailable = 5;
+    int doubleAvailable = 3;
+    int suiteAvailable = 2;
+
+    // Output
+        System.out.println("Hotel Room Initialization\n");
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        System.out.println("Double Room:");
+        doubleroom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
+}
 }
